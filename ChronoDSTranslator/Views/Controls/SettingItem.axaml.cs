8806439936306d47fc2ebe08a7b572f9e17e98ef
whivel <whivel>
2025-02-17 21:57:46 +0100

    feat: creato SettingItem per permettere la configurazione delle tabelle di conversione

---
+++ ChronoDSTranslator/Views/Controls/SettingItem.axaml.cs
@@ -0,0 +1,7 @@
+﻿using Avalonia.Controls;
+
+namespace ChronoDSTranslator.Views.Controls;
+
+public class SettingItem : ContentControl
+{
+}
---
+++ ChronoDSTranslator/Views/Controls/ToolbarButton.axaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace ChronoDSTranslator.Views.Controls;
+
+public partial class ToolbarButton : UserControl
+{
+    private static readonly StyledProperty<IImage> IconProperty =
+        AvaloniaProperty.Register<ToolbarButton, IImage>(nameof(Icon));
+
+    private static readonly StyledProperty<bool> IsSelectedProperty =
+        AvaloniaProperty.Register<ToolbarButton, bool>(nameof(IsSelected));
+
+
+    public ToolbarButton()
+    {
+        InitializeComponent();
+    }
+
+    public IImage Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public bool IsSelected
+    {
+        get => GetValue(IsSelectedProperty);
+        set => SetValue(IsSelectedProperty, value);
+    }
+}
